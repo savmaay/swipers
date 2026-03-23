636aec01a8f0c5a41634e434fdcbda3b0b814e4f
Nancy Vutuan <nancyvutuan@gmail.com>
2026-03-23 16:50:53 -0400

    update transporter

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,10 +8,15 @@ const auth = require('../middleware/auth');
 const User = require('../models/User.js');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS 
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false 
   }
 });
 
